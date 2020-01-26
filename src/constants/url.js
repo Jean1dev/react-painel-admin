8b23115adb3344d6deac59332de5cb0f0f00aaac
Jean <jeanlucafp@gmail.com>
2020-01-26 18:38:44 -0300

    criado CI para deploy no heroku

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,4 +1,4 @@
-const prodUrl = ''
+const prodUrl = 'https://if-api-gateway.herokuapp.com'
 const localUrl = 'http://localhost:8080'
 const localWindowsUrl = 'http://192.168.1.2:8080'
 let baseUrl = process.env.NODE_ENV === 'development' ? localUrl : prodUrl
